f06c5dc382a83541f0d6e17cbc0c5522d798ce5b
laetitiaguerout <guerout.lg@gmail.com>
2023-04-21 21:19:03 +0200

    ca marche pas encore mais je verrai demain

--- non_linear_ls.py
+++ non_linear_ls.py
@@ -22,19 +22,30 @@ ni=len(Item)
 
 def alsq(nbrIter,nbrFeatures,lmbd,R,test):
 
-    X = np.ones((nu, nbrFeatures))
+    X =np.ones((nu, nbrFeatures))
     Y = np.ones((nbrFeatures, ni))
-    W = R > 0
+    W0 = R > 0
+    W=np.zeros(np.shape(R))
+    for row in range(len(R)):
+        for column in range(len(R[row])):
+            if W0[row,column]==True:
+                W[row,column]=1
+            else:
+                W[row,column]=0
+    # print(W)
     MAE_alsq = []
 
     for ii in range(nbrIter):
         for u in range(nu):
             # Compute the least-square problem for Xu
-            Xu=(np.linalg.inv(2*Y@np.diag(W[u,:])@Y.T+2*lmbd)@2*Y@np.diag(W[u,:])@R[u,:].T).T
+            n=np.shape((2*Y@np.diag(W[u,:])@Y.T))[0]
+            Xu=((np.linalg.inv((2*Y)@np.diag(W[u,:])@Y.T+2*lmbd*np.identity(n)))@(2*Y)@(np.diag(W[u,:]))@(R[u,:].T)).T
+            print(Xu)
             np.linalg.lstsq(Xu,Y)
         for i in range(ni):
             # Compute the least-square problem for Yi
-            Yi=np.linalg.inv(2*X.T@np.diag(W[:,i])@X+2*lmbd)@2*R[:,i].T@np.diag(W[:,i])@X
+            n=np.shape((2*X.T@np.diag(W[:,i])@X))[0]
+            Yi=np.linalg.inv((2*X.T)@np.diag(W[:,i])@X+2*lmbd*np.identity(n))@(2*R[:,i].T)@np.diag(W[:,i])@X
             np.linalg.lstsq(X,Yi)
         prediction = [np.dot(X[(r['user']-1),:],Y[:,(r['movie']-1)])
         for r in test]
@@ -45,4 +56,6 @@ def alsq(nbrIter,nbrFeatures,lmbd,R,test):
 
         print(ii, np.mean(np.abs(errorRating)))
         
-    return X,Y, MAE_alsq
+    return X,Y,MAE_alsq
+
+alsq(5,2,0.05,R,testUserItem)
